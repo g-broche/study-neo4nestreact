085b7893cf5f7503b982ed8d918e289d46e8cfb4
Greg <gregory.broche@yahoo.fr>
2024-11-15 17:44:45 +0100

    added new classes to Entity type valid options

--- apps/api/src/type/entity.ts
+++ apps/api/src/type/entity.ts
@@ -1,6 +1,10 @@
+import { Category } from 'src/entity/category';
 import { Game } from 'src/entity/game';
 import { Genre } from 'src/entity/genre';
+import { Platform } from 'src/entity/platform';
+import { Tag } from 'src/entity/tag';
 import { Type } from 'src/entity/type';
+import { Video } from 'src/entity/video';
 import {
   CategoryDTO,
   GameDTO,
@@ -11,7 +15,7 @@ import {
   VideoDTO,
 } from 'src/interface/dataTransfertObject';
 
-export type Entity = Genre | Game | Type;
+export type Entity = Genre | Game | Type | Category | Tag | Platform | Video;
 export type EntityDTO =
   | GenreDTO
   | GameDTO
